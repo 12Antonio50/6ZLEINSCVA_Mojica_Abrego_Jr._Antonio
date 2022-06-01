8df6905ec9a8b58225a9eb534d5983c73b6fa306
12Antonio50 <ja.mojica.a@edu.utc.mx>
2022-06-01 13:20:40 -0500

    Lista completa

---
+++ 04Lista/OtraLista.cpp
@@ -0,0 +1,172 @@
+//lista con apuntadores
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <iostream>
+
+/*
+stdio es la libreria estandar de ++
+para manejo de entradas y salidas en consola 
+
+stdlib es la libreria estandar de estructuras 
+de datos en c++ ára listas, pilas, cilas, árbol y grafos 
+
+conio es la libreria estandar para el manejo de manipulacion de la pantalla, pausar, colores,
+manipulacion de ventanas
+*/
+
+using namespace std;
+//definicion de la lita 
+
+struct dato{
+	int i;
+	dato *s;//nodo siguiente
+}*a, *i, *p, *e;
+
+//metodos
+int buscar(int d);
+void insertar(void);
+void mostrar(void);
+void borrar(void);
+void menu(void);
+//void guardar(void);
+//void cargar (void);
+
+//principal
+main(){
+	menu()
+}
+//menu
+
+void menu(void){
+	int opc, da;
+	do{
+		
+		//imprimir menu
+		cout<<"Ejemplo de una lista con apuntadores"<<endl;
+		cout<<"\n1. buscar dato";
+		cout<<"\n2. insertar dato";
+		cout<<"\n3. mostrar dato";
+		cout<<"\n4. borrar dato";
+		cout<<"\n5. guardar dato en un archivo";
+		cout<<"\n6. cargar dato en un archivo";
+		cout<<"\n7. salir";
+		cout<<"\n1. Seleccione la opcion deseada";
+		cin>>opc;
+		
+		switch(opc){
+			case 1:
+				//buscar
+				cout<<"\n Ingresa el dato a buscar";
+				cin>>da;
+				if(buscar(da)){
+					cout<<"Dato si existe";
+				}else{
+					cout<<"El dato no existe";
+					getch(); //pausa
+				}
+				break;
+			case 2:
+				cout<<"\n Ingresa el dato";
+				cin>>da;
+				insertar(da);
+				break;
+			case 3:
+				//mostrar
+				mostrar();
+				break;
+			case 4:
+				//borrar
+				borrar();
+				break:
+			case 5:
+				//guardar
+				guardar();
+				break;
+			case 6:
+				//cargar
+				cargar();
+				break;
+			case 7:
+				//salir
+				cout<<"Adios";
+				getch;
+				//antes de salir del progroma hay que borrar todos los datos de la lita
+			
+				p = i;
+				while(p){
+					a = p;
+					p = p->s;
+					delete(a);	
+				}
+				//salir 
+				exit(0);
+				
+			default:
+					cout<<"Opcion invalida";
+					getch();
+		}
+	}while(opc);
+}
+
+void mostrar(void){
+	int cont = 1;
+	//saber si hay lista
+	if(!i){
+		cout<<"No hay lista para mostrar";
+		getch();
+		return;
+	}
+	p = i;
+	cout<<"\n\n";
+	/*como si ecxiste un dato para la lista tenemos que empezar a recorrerla 
+	por cada uno de los elementos 
+	*/
+	while(p){
+		cout<<cont++<<"\nValor : "<<p->i<<endl;
+		p = p -> s;
+	}
+	cout<<"\n Fin de la lista";
+	getch();
+	
+}
+//buscar
+int buscar(int d){
+		//saber si hay lista
+	if(!i){
+		cout<<"No hay en la lista para mostrar";
+		getch();
+		return;
+	}
+	//si hay 
+	p = i;
+	a = NULL;
+	
+	while(p ->s  && p ->i < d){
+		//recorrer los valoeres de la lista 
+		a = p;
+		p = P->s;
+	}
+	return (p->i == d?1:0);
+}
+/*
+ para insertar cuantos caminos hay?
+ 1. la primera vez que se inserta un dato
+  se debe de crear el nodo y verificar los apuntadores 
+2. enmedio 
+ se debe de buscar donde se va a ingresar debe de acomodar el 
+ apuntador anterior y el apuntador siguiente (apuntador que apunta)
+3. final solo se apunta y se ajusta el apuntador 
+
+*/
+
+
+
+
+
+
+
+
+
+
